5646f7076c78aa03a0be4a5db9599b9492144c98
Thompson, Jeff M <jeff.m.thompson@optum.com>
2019-03-21 11:34:10 -0600

    Finished the checkout process
    Added code to finish the check out process, added some more validation tasks. Updated Feature and Scenerio descriptions.

--- build/resources/main/features/saucedemo.feature
+++ build/resources/main/features/saucedemo.feature
@@ -1,11 +1,23 @@
-Feature: Page Loading
-  As a user,
-  I want to load the page, type some text and check the behavior.
+Feature: Sauce Labs shopping demo
+  1. Use the website https://www.saucedemo.com/ as the test site
+  2. Create a test that logs in as the user: standard_user password: secret_sauce
+  3. Add the “Sauce Labs Onesie” to the cart
+  4. Add the “Sauce Labs Bike Light” to the cart
+  5. Verify both items are in the cart
 
-  Scenario: Simple page loading
+  @demo
+  Scenario: SauceDemo
 
     Given I navigate to "https://www.saucedemo.com/"
     Then log in as user "standard_user" with password "secret_sauce"
     Then I add “Sauce Labs Onesie” to the cart
     Then I add “Sauce Labs Bike Light” to the cart
-    Then I validate both items are in the cart
+    Then I click the shopping cart
+    And I validate both items are in the cart
+    Then I complete the checkout process
+    Then Fill in the shipping details with First name "Test" Last name "User" and Zipcode "12345" and click Continue
+    #For the following step use Numbers only for the amounts, no dollar sign or symbols
+    And I validate shipping method is "FREE PONY EXPRESS DELIVERY!"  Item total is "17.98" tax is "1.44" and Total is "19.42"
+    Then I click Finish
+    And I validate I am on the URL "https://www.saucedemo.com/checkout-complete.html"
+
--- src/main/java/saucedemo/Step_Definitions.java
+++ src/main/java/saucedemo/Step_Definitions.java
@@ -2,8 +2,11 @@ package saucedemo;
 
 
 import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
+import cucumber.api.java.sl.In;
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -47,12 +50,72 @@ public class Step_Definitions {
 
     @Then("^I validate both items are in the cart$")
     public void iValidateBothItemsAreInTheCart() throws InterruptedException {
-        //Click the shopping cart icon in the upper right of the screen
-        Hooks.driver.findElement(By.xpath("//*[@id='shopping_cart_container']")).click();
+
         //
         assertTrue(Hooks.explicitWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='item_0_title_link']/div"))).isDisplayed());
         assertTrue(Hooks.explicitWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='item_0_title_link']/div"))).isDisplayed());
-    
 
+
+    }
+
+    @Then("I click the shopping cart")
+    public void iClickTheShoppingCart() {
+        //Click the shopping cart icon in the upper right of the screen
+        WebElement cart = Hooks.explicitWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='shopping_cart_container']")));
+        cart.click();
+    }
+    @Then("I complete the checkout process")
+    public void iCompleteTheCheckoutProcess() {
+        //Click the checkout button in the bottom right
+        WebElement cart = Hooks.explicitWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='cart_contents_container']/div/div[contains(@class,'cart_footer')]/a[contains(@class,'btn_action')]")));
+        cart.click();
+
+    }
+
+
+    @Then("Fill in the shipping details with First name {string} Last name {string} and Zipcode {string} and click Continue")
+    public void fillInTheShippingDetailsWithFirstNameLastNameAndZipcodeAndClickContinue(String firstName, String lastName, String zipCode) {
+        //first name input
+        WebElement first = Hooks.explicitWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='first-name']")));
+        first.sendKeys(firstName);
+        //last name input
+        WebElement last = Hooks.explicitWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='last-name']")));
+        last.sendKeys(lastName);
+        //zipcode input
+        WebElement zip = Hooks.explicitWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='postal-code']")));
+        zip.sendKeys(zipCode);
+        //continue button
+        Hooks.driver.findElement(By.xpath("//*[@id='checkout_info_container']/div/form/div[contains(@class,'checkout_buttons')]/input")).click();
+    }
+
+    @And("I validate shipping method is {string}  Item total is {string} tax is {string} and Total is {string}")
+    public void iValidateCheckoutInfo(String shippingMethod, String itemTotal, String tax, String total) {
+
+        //TODO parametrize the xpaths here to remove the hard codeded expected values
+        //Validate Shipping method
+        System.out.println("wait here");
+        assertTrue(Hooks.explicitWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='checkout_summary_container']/div/div[contains(@class,'summary_info')]/div[text()='"+ shippingMethod +"']"))).isDisplayed());
+        //Validate Item Total (Number ONLY no dollar sign)
+        assertTrue(Hooks.explicitWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='checkout_summary_container']/div/div[contains(@class,'summary_info')]/div[text()='"+ itemTotal +"']"))).isDisplayed());
+        //Validate Tax(Number ONLY no dollar sign)
+        assertTrue(Hooks.explicitWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='checkout_summary_container']/div/div[contains(@class,'summary_info')]/div[text()='" + tax + "']"))).isDisplayed());
+        //Validate Total(Number ONLY no dollar sign)
+        assertTrue(Hooks.explicitWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='checkout_summary_container']/div/div[contains(@class,'summary_info')]/div[text()='"+ total +"']"))).isDisplayed());
+
+
+
+    }
+
+    @Then("I click Finish")
+    public void iClickFinish() {
+        //Click the finish button
+        Hooks.driver.findElement(By.xpath("//*[@id='checkout_summary_container']/div/div/div/a[contains(@class,'btn_action cart_button')]")).click();
+
+    }
+
+    @And("I validate I am on the URL {string}")
+    public void iValidateIAmOnTheURL(String urlToValidate) {
+        String currentUrl = Hooks.driver.getCurrentUrl();
+        Assert.assertEquals(currentUrl, urlToValidate );
     }
 }
